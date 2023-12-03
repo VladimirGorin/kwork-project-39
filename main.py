1e70e08c0e7f0eb9f7af02a2449b1c690c6a8061
Владимир Горин <vladimirgorin410@gmail.com>
2023-12-03 13:46:00 +0300

    first

---
+++ main.py
@@ -0,0 +1,67 @@
+import logging
+import shutil
+
+from selenium import webdriver
+import platform
+from selenium.webdriver.chrome.options import Options
+
+from utils import send_messages, auth
+
+shutil.copy2('./logs/whatsapp_bot.log', './logs/whatsapp_bot_backup.log')
+open('./logs/whatsapp_bot.log', 'w').close()
+
+logging.basicConfig(filename='./logs/whatsapp_bot.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+logging.info("Script started")
+
+def create_browser():
+    system_platform = platform.system()
+
+    chrome_options = Options()
+    chrome_options.add_argument("user-data-dir=sessions/whatsapp") 
+    chrome_options.add_argument("disable-gpu")
+
+    if system_platform == "Windows":
+        chromedriver_path = "./chromedriver/windows/chromedriver.exe"
+        browser = webdriver.Chrome(chromedriver_path, options=chrome_options)
+        return browser
+    elif system_platform == "Linux":
+        browser = webdriver.Chrome(options=chrome_options)
+        return browser
+    else:
+        raise Exception("Unsupported operating system")
+
+
+
+try:
+    print("\n[1] Авторизация")
+    print("[2] Отправка сообщений")
+    print("[3] Повторная проверка и отправка сообщения\n")
+
+    choice = input("Выберите вариант (1, 2 или 3): ")
+    browser = create_browser()
+    browser.set_window_position(-10000, 0)
+    
+    if choice.isdigit():
+        choice = int(choice)
+        if choice == 1:
+            browser.get("https://web.whatsapp.com/")
+            auth.authenticate()
+
+        elif choice == 2:
+            send_messages.send_messages(browser)
+
+        elif choice == 3:
+            send_messages.send_rechecks_message(browser)
+
+        else:
+            print("\nНеверный вариант. Пожалуйста, выберите 1, 2 или 3.\n")
+            logging.warning("User entered an invalid option")
+    else:
+        print("\nНеверный ввод. Пожалуйста, введите число.\n")
+        logging.warning("User entered a non-numeric input")
+
+except Exception as e:
+    logging.error(f"An error occurred: {str(e)}", exc_info=True)
+
+finally:
+    logging.info("Script ended")
---
+++ utils/queries.py
@@ -0,0 +1,1 @@
+send_message_body_id = "main"
